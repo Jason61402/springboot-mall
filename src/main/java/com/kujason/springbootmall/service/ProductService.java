4d0493294d45bb44d6a59dfe604250a3058e3e0b
Jason <j22894@gmail.com>
2023-10-19 19:28:27 +0800

    商品功能 - 實作查詢商品列表-基礎實作

--- src/main/java/com/kujason/springbootmall/service/ProductService.java
+++ src/main/java/com/kujason/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.kujason.springbootmall.service;
 import com.kujason.springbootmall.dto.ProductRequest;
 import com.kujason.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
